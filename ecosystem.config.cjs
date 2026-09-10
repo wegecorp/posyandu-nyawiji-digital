4c7bab97c25d23f143c58186929bbb37bf8944d8
wegecorp <wegecorp@users.noreply.github.com>
2026-09-10 16:12:36 +0700

    forkks

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: `start -p ${PORT}`,
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       max_memory_restart: '500M',
       env: {

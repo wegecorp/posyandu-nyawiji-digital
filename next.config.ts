d77f2f736d702638bf63a0baafcc97915103bfbf
wegecorp <wegecorp@users.noreply.github.com>
2026-09-10 16:39:50 +0700

    domain

--- next.config.ts
+++ next.config.ts
@@ -28,7 +28,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
+            value: 'camera=(self), microphone=(), geolocation=()',
           },
         ],
       },
